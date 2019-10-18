6aebc26ef817c77344031a28a1a3b5e587e2ab38
aritab <17816878365@163.com>
2019-10-18 19:30:27 +0800

    线程池

--- lib/threadpool.h
+++ lib/threadpool.h
@@ -24,7 +24,21 @@ class threadpool{
 };
 
 template<typename T>
-threadpool<T>::threadpool(int thread_number, int max_requests):
+threadpool<T>::threadpool(int thread_number, int max_requests):m_thread_number(thread_number),m_max_requests(max_requests),m_stop(false),m_threads(NULL){
+	if((thread_number<=0)||(max_requests<=0)){
+		throw std::exception();
+	}
+	m_threads=new pthread_t(m_thread_number);
+	if(!m_threads){
+		throw std::exception();
+	}
+	for(int i=0;i<thread_number;++i){
+		printf("create the %dth thread\n",i);
+		if(pthread_create(m_threads+i,NULL,worker,this)!=0){
+			
+		}
+	}
+}
 
 
 
